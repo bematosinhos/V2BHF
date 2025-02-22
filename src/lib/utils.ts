364cd014f1095bcc752ddd43c3772f9228084627
Allan Baptista <allan@beanimus.com>
2025-02-22 15:36:04 -0300

    initial setup

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/**
+ * A utility function that merges Tailwind CSS classes with the clsx utility.
+ * @param inputs - The Tailwind CSS classes to merge.
+ * @returns The merged Tailwind CSS classes.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
